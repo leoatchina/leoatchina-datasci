49e3111207c1dba17854ec80b016f17262394b2b
leoatchina <leoatchina@gmail.com>
2017-12-02 09:37:55 +0800

    fixed errors

--- jupyter_lab_config.py
+++ jupyter_lab_config.py
@@ -1,5 +1,5 @@
 c.NotebookApp.ip = '*' #任何ip都可以
 c.NotebookApp.open_browser = False
 c.NotebookApp.port = 8888# 设置未被使用的端口
-c.NotebookApp.notebook_dir = u'/work/' # Notebook启动目录
+c.NotebookApp.notebook_dir = u'/jupyter/' # Notebook启动目录
 # c.NotebookApp.password = 'sha1:0b5beb55d8f0:6c15ed8bcf394fba574a82bb56ef458c724d331a' #jupyter
