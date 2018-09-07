2d65908acc037970a1d73375285274cd4e84ce1f
leoatchina <leoatchina@gmail.com>
2018-09-07 18:02:00 +0800

    update

--- jupyter_notebook_config.py
+++
@@ -1,5 +0,0 @@
-c.NotebookApp.ip = '*' #任何ip都可以
-c.NotebookApp.open_browser = False
-c.NotebookApp.port = 7777# 设置未被使用的端口
-c.NotebookApp.notebook_dir = u'/jupyter/' # Notebook启动目录
-# c.NotebookApp.password = 'sha1:0b5beb55d8f0:6c15ed8bcf394fba574a82bb56ef458c724d331a' #jupyter
