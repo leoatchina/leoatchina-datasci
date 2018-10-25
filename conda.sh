a7680ab3ee4497b763e794dcbccb5d1689afad0b
leoatchina <leoatchina@gmail.com>
2018-10-25 18:08:23 +0800

    change

--- conda.sh
+++ conda.sh
@@ -1,3 +1,4 @@
-conda create -p /jupyter/bioinfo python=2
-curl http://data.biostarhandbook.com/install/conda.txt | xargs conda install -p /jupyter/bioinfo -y
-conda install -p /jupyter/bioinfo trim-galore deeptools homer meme macs2 bowtie sambamba multiqc
+conda create -p /jupyter/bioinfo python=2 
+conda install -p /jupyter/bioinfo -y bamtools bbmap bcftools bedtools bioawk bowtie bowtie2 bwa cutadapt datamash emboss entrez-direct fastqc freebayes hisat2 htslib parallel=20180922 perl-list-moreutils picard samtools seqtk snpeff sra-tools subread trimmomatic trim-galore sambamba homer meme
+# make a local path to /opt/anaconda3/pkgs for beneath install
+conda install -p /jupyter/bioinfo -y deeptools blast multiqc macs2
