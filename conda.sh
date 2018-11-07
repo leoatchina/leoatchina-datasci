044c632a22317d4fa8344313b3eab597272a5d71
leoatchina <leoatchina@gmail.com>
2018-11-07 12:27:57 +0800

    update

--- conda.sh
+++ conda.sh
@@ -1,4 +1,2 @@
-conda create -p /jupyter/bioinfo python=2 
-conda install -p /jupyter/bioinfo -y bamtools bbmap bcftools bedtools bioawk bowtie bowtie2 bwa cutadapt datamash emboss entrez-direct fastqc freebayes hisat2 htslib parallel=20180922 perl-list-moreutils picard samtools seqtk snpeff sra-tools subread trimmomatic trim-galore sambamba homer meme
-# make a local path to /opt/anaconda3/pkgs for beneath install
-conda install -p /jupyter/bioinfo -y deeptools blast multiqc macs2
+conda create -p /jupyter/bioinfo python=2.7 -y
+conda install -p /jupyter/bioinfo -y bamtools bbmap bcftools bedtools bioawk bowtie bowtie2 bwa cutadapt datamash emboss entrez-direct fastqc freebayes hisat2 htslib parallel=20180922 perl-list-moreutils picard samtools seqtk snpeff sra-tools subread trimmomatic trim-galore sambamba homer meme vcftools deeptools blast multiqc macs2
