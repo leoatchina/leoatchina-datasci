5946059ec2150f143217763981d7d5c12803def8
leoatchina <leoatchina@gmail.com>
2019-02-27 17:04:56 +0800

    add -A

--- pkgs.R
+++ pkgs.R
@@ -1,6 +1,9 @@
 # for some reasons, you should install this packages at first
-options(encoding = 'UTF-8')
-options("repos" = c(CRAN="https://mirrors.ustc.edu.cn/CRAN"))
+local({
+  options(encoding = 'UTF-8')
+  options( repos  = "https://mirrors.ustc.edu.cn/CRAN/" )
+  options( BioC_mirror = "https://mirrors.ustc.edu.cn/bioc/" )
+})
 require_packages = c(
   "AlgDesign",
   "ape",
@@ -133,9 +136,9 @@ require_packages = c(
   "zip",
   "zoo"
 )
-
 install_packages = setdiff(require_packages,unname(installed.packages()[,1]))
 if(length(install_packages)){install.packages(install_packages)}
+
 # install from github
 library(devtools)
 devtools::install_github("rstudio/addinexamples")
@@ -144,27 +147,42 @@ devtools::install_github("thomasp85/ggforce")
 devtools::install_github("GuangchuangYu/DOSE")
 devtools::install_github("GuangchuangYu/enrichplot")
 devtools::install_github("GuangchuangYu/clusterProfiler")
-                       
-# bioconductor
-# source('https://bioconductor.org/biocLite.R')
-if (!requireNamespace("BiocManager", quietly=TRUE))
-    install.packages("BiocManager")
-options(BioC_mirror="https://mirrors.ustc.edu.cn/bioc")
-## modern install 
-BiocManager::install("GDCRNATools", suppressUpdates=TRUE, suppressAutoUpdate=TRUE)
-BiocManager::install(c("DESeq2" , "edgeR"), suppressUpdates=TRUE, suppressAutoUpdate=TRUE) ##)
-BiocManager::install(c("fgsea","org.Mm.eg.db", "org.Hs.eg.db","GEOquery", "limma", "simpleaffy", "AnnotationDbi", "biomartr"),suppressUpdates=TRUE, suppressAutoUpdate=TRUE)
-
-BiocManager::install("RTCGA", suppressUpdates=TRUE, suppressAutoUpdate=TRUE)
-BiocManager::install("RTCGA.clinical", suppressUpdates=TRUE, suppressAutoUpdate=TRUE) ## 14Mb
-BiocManager::install('RTCGA.rnaseq', suppressUpdates=TRUE, suppressAutoUpdate=TRUE) ##  (612.6 MB)
-BiocManager::install("RTCGA.mRNA", suppressUpdates=TRUE, suppressAutoUpdate=TRUE) ##  (85.0 MB)
-BiocManager::install('RTCGA.mutations', suppressUpdates=TRUE, suppressAutoUpdate=TRUE)  ## (103.8 MB)
-# legacy install
-# Install the main RTCGA package
-#BiocManager::biocLite("RTCGA", suppressUpdates=TRUE, suppressAutoUpdate=TRUE)
-# Install the clinical and mRNA gene expression data packages
-#BiocManager::biocLite("RTCGA.clinical", suppressUpdates=TRUE, suppressAutoUpdate=TRUE) ## 14Mb
-#BiocManager::biocLite('RTCGA.rnaseq', suppressUpdates=TRUE, suppressAutoUpdate=TRUE) ##  (612.6 MB)
-#BiocManager::biocLite("RTCGA.mRNA", suppressUpdates=TRUE, suppressAutoUpdate=TRUE) ##  (85.0 MB)
-#BiocManager::biocLite('RTCGA.mutations', suppressUpdates=TRUE, suppressAutoUpdate=TRUE)  ## (103.8 MB)
+                 
+bioPackages = 
+  c( 
+    "GDCRNATools", ##
+    "dplyr", "stringi", "purrr", ## 
+    "R.utils", "data.table", ## unzip and read table
+    "GEOquery", ## download
+    "FactoMineR", "factoextra", "ggfortify", ## PCA
+    "pheatmap", ## heatmap
+    "ggplot2", ## Volcano plot
+    "limma", "DESeq2", "edgeR", ## DEG
+    "clusterProfiler", "org.Hs.eg.db", "org.Mm.eg.db" ## annotation
+    "pathview", ## kegg
+    "RTCGA", "RTCGA.clinical", "RTCGA.rnaseq", "RTCGA.mRNA", "RTCGA.mutations" ## TCGA
+  )
+lapply( bioPackages, 
+  function(bioPackage) {
+    if ( !require( bioPackage, character.only = T ) ) {
+      CRANpackages = available.packages()
+      ## install packages by CRAN
+      if ( bioPackage %in% rownames( CRANpackages ) ) {
+        install.packages( bioPackage )
+      }else{
+        ## install packages by bioconductor
+        ## R version >= 3.5 ===> BiocManager
+        if ( as.character( sessionInfo()$R.version$minor ) >= 3.5 ) {
+          if (!requireNamespace("BiocManager", quietly = TRUE))
+            install.packages("BiocManager")
+          BiocManager::install(bioPackage, update = TRUE, ask = FALSE)
+        }else{
+          ## R version < 3.5 ===> BiocInstaller
+          if (!requireNamespace("BiocInstaller", quietly = TRUE))
+            source( "https://bioconductor.org/biocLite.R" )
+          BiocInstaller::biocLite( bioPackage, ask = FALSE)
+        }
+      }
+    }
+  }
+)
