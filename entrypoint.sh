b2329f26a893ce0666b432b69e1170dc03ced6ee
leoatchina <leoatchina@gmail.com>
2019-04-29 19:34:11 +0800

    add test.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ echo "c.NotebookApp.password = '$SHA1'">>/opt/config/jupyter_lab_config.py
 # passwd for rserver
 echo rserver:$PASSWD | chpasswd
 # passwd for code-server
-echo "command=/opt/code-server/code-server -P '$PASSWD' -d /jupyter/.config/.vscode -e /root/.config/.vscode-extentions">>/opt/config/supervisord.conf
+echo "command=/opt/code-server/code-server -P '$PASSWD' -d /root/.config/.vscode -e /root/.config/.vscode-extentions">>/opt/config/supervisord.conf
 /usr/bin/supervisord -c /opt/config/supervisord.conf
