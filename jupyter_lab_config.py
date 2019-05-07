dd6c0d8effa096f4d7d5745b531797e5eb32c561
leoatchina <leoatchina@gmail.com>
2019-05-07 17:10:32 +0800

    update jupyerlab start setting

--- jupyter_lab_config.py
+++ jupyter_lab_config.py
@@ -1,4 +1,4 @@
-c.NotebookApp.ip = '*' #任何ip都可以
+c.NotebookApp.ip = '0.0.0.0' #任何ip都可以
 c.NotebookApp.open_browser = False
 c.NotebookApp.port = 8888# 设置未被使用的端口
 c.NotebookApp.notebook_dir = u'/jupyter/' # Notebook启动目录
