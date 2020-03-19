f6031e8304ad974bf3fca6721398bec03a07f09c
leoatchina <leoatchina@outlook.com>
2020-03-19 06:00:53 +0000

    update

--- scripts_install/jupyter_lab_extensions.sh
+++ scripts_install/jupyter_lab_extensions.sh
@@ -3,5 +3,4 @@ jupyter labextension install @jupyterlab/toc
 jupyter labextension install jupyterlab-kernelspy 
 jupyter labextension install jupyterlab-spreadsheet 
 jupyter labextension install @krassowski/jupyterlab_go_to_definition
-jupyter labextension install jupyterlab_vim
 jupyter lab build 
