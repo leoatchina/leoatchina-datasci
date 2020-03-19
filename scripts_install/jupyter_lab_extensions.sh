12bc8c6ea5d36354e0f2552c3a5bfc85f9b22b61
leoatchina <leoatchina@outlook.com>
2020-03-19 05:44:45 +0000

    update

--- scripts_install/jupyter_lab_extensions.sh
+++ scripts_install/jupyter_lab_extensions.sh
@@ -1,12 +1,7 @@
-jupyter labextension install @jupyter-widgets/jupyterlab-manager
 jupyter labextension install ipysheet
 jupyter labextension install @jupyterlab/toc 
-jupyter labextension install jupyterlab-drawio
 jupyter labextension install jupyterlab-kernelspy 
 jupyter labextension install jupyterlab-spreadsheet 
 jupyter labextension install @krassowski/jupyterlab_go_to_definition
-jupyter labextension install @telamonian/theme-darcula
-jupyter labextension install @mohirio/jupyterlab-horizon-theme
 jupyter labextension install jupyterlab_vim
-jupyter labextension install @lckr/jupyterlab_variableinspector
 jupyter lab build 
