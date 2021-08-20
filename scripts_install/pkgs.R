5c95b4ac8d04688366d64c8f2256ae3ddd0d1abd
leoatchina <leoatchina@outlook.com>
2021-08-20 23:02:00 +0800

    update pkgs.R

--- scripts_install/pkgs.R
+++ scripts_install/pkgs.R
@@ -4,136 +4,136 @@ options("repos" = c(CRAN="https://mirrors.tuna.tsinghua.edu.cn/CRAN/"))
 options(BioC_mirror="https://mirrors.tuna.tsinghua.edu.cn/bioconductor")
 
 require_packages = c(
-  "AlgDesign",
-  "ape",
-  "arules",
-  "BayesTree",
-  "Ball", # for debug
-  "bmp",
-  "Cairo",
-  "car",
-  "chron",
-  "crayon",
-  "data.table",
-  "DBI",
-  "devtools",
-  "downloader",
-  "dplyr",
-  "DT",
-  "e1071",
-  "emdbook",
-  "evd",
-  "expm",
-  "fields",
-  "fmsb",
-  "foreign",
-  "formatR",
-  "formattable",
-  "fortunes",
-  "gafit",
-  "gcookbook",
-  "getopt",
-  "GGally",
-  "ggiraph",
-  "ggmap",
-  "ggplot2",
-  "glmnet",
-  "googleVis",
-  "gridExtra",
-  "gsubfn",
-  "gtable",
-  "gvlma",
-  "gWidgets",
-  "Hmisc",
-  "htmltools",
-  "IDPmisc",
-  "igraph",
-  "IRdisplay",
-  "jpeg",
-  "kableExtra",
-  "knitr",
-  "kohonen",
-  "lars",
-  "lattice",
-  "lubridate",
-  "magic",
-  "magrittr",
-  "MASS",
-  "mcmc",
-  "microbenchmark",
-  "miniUI",
-  "mosaic",
-  "mvnormtest",
-  "nnet",
-  "nortest",
-  "officer",
-  "openxlsx",
-  "optparse",
-  "outliers",
-  "pheatmap",
-  "pixmap",
-  "plotrix",
-  "plyr",
-  "png",
-  "pool",
-  "pracma",
-  "pROC",
-  "psych",
-  "qualityTools",
-  "quantmod",
-  "R.matlab",
-  "R.utils",
-  "R6",
-  "Rcpp",
-  "repr",
-  "readxl",
-  "reshape2",
-  "RgoogleMaps",
-  "rhandsontable",
-  "rJava",
-  "rjson",
-  "rmarkdown",
-  "rms",
-  "RMySQL",
-  "robust",
-  "robustbase",
-  "RODBC",
-  "rredis",
-  "rsconnect",
-  "Rserve",
-  "RSQLite",
-  "rstudioapi",
-  "rticles",
-  "RUnit",
-  "rvest",
-  "rworldmap",
-  "scatterplot3d",
-  "sciplot",
-  "shiny",
-  "shinyAce",
-  "shinyBS",
-  "shinycssloaders",
-  "shinydashboard",
-  "shinyjs",
-  "shinythemes",
-  "sjmisc",
-  "sos",
-  "sound",
-  "spam",
-  "sqldf",
-  "stringi",
-  "stringr",
-  "tidyverse",
-  "treemapify",
-  "vars",
-  "vcd",
-  "venneuler",
-  "wordcloud",
-  "xlsx",
-  "XML",
-  "xts",
-  "zip",
-  "zoo"
+                     "AlgDesign",
+                     "ape",
+                     "arules",
+                     "BayesTree",
+                     "Ball", # for debug
+                     "bmp",
+                     "Cairo",
+                     "car",
+                     "chron",
+                     "crayon",
+                     "data.table",
+                     "DBI",
+                     "devtools",
+                     "downloader",
+                     "dplyr",
+                     "DT",
+                     "e1071",
+                     "emdbook",
+                     "evd",
+                     "expm",
+                     "fields",
+                     "fmsb",
+                     "foreign",
+                     "formatR",
+                     "formattable",
+                     "fortunes",
+                     "gafit",
+                     "gcookbook",
+                     "getopt",
+                     "GGally",
+                     "ggiraph",
+                     "ggmap",
+                     "ggplot2",
+                     "glmnet",
+                     "googleVis",
+                     "gridExtra",
+                     "gsubfn",
+                     "gtable",
+                     "gvlma",
+                     "gWidgets",
+                     "Hmisc",
+                     "htmltools",
+                     "IDPmisc",
+                     "igraph",
+                     "IRdisplay",
+                     "jpeg",
+                     "kableExtra",
+                     "knitr",
+                     "kohonen",
+                     "lars",
+                     "lattice",
+                     "lubridate",
+                     "magic",
+                     "magrittr",
+                     "MASS",
+                     "mcmc",
+                     "microbenchmark",
+                     "miniUI",
+                     "mosaic",
+                     "mvnormtest",
+                     "nnet",
+                     "nortest",
+                     "officer",
+                     "openxlsx",
+                     "optparse",
+                     "outliers",
+                     "pheatmap",
+                     "pixmap",
+                     "plotrix",
+                     "plyr",
+                     "png",
+                     "pool",
+                     "pracma",
+                     "pROC",
+                     "psych",
+                     "qualityTools",
+                     "quantmod",
+                     "R.matlab",
+                     "R.utils",
+                     "R6",
+                     "Rcpp",
+                     "repr",
+                     "readxl",
+                     "reshape2",
+                     "RgoogleMaps",
+                     "rhandsontable",
+                     "rJava",
+                     "rjson",
+                     "rmarkdown",
+                     "rms",
+                     "RMySQL",
+                     "robust",
+                     "robustbase",
+                     "RODBC",
+                     "rredis",
+                     "rsconnect",
+                     "Rserve",
+                     "RSQLite",
+                     "rstudioapi",
+                     "rticles",
+                     "RUnit",
+                     "rvest",
+                     "rworldmap",
+                     "scatterplot3d",
+                     "sciplot",
+                     "shiny",
+                     "shinyAce",
+                     "shinyBS",
+                     "shinycssloaders",
+                     "shinydashboard",
+                     "shinyjs",
+                     "shinythemes",
+                     "sjmisc",
+                     "sos",
+                     "sound",
+                     "spam",
+                     "sqldf",
+                     "stringi",
+                     "stringr",
+                     "tidyverse",
+                     "treemapify",
+                     "vars",
+                     "vcd",
+                     "venneuler",
+                     "wordcloud",
+                     "xlsx",
+                     "XML",
+                     "xts",
+                     "zip",
+                     "zoo"
 )
 install_packages = setdiff(require_packages,unname(installed.packages()[,1]))
 if(length(install_packages)){install.packages(install_packages)}
@@ -146,11 +146,11 @@ devtools::install_github("thomasp85/ggforce")
 devtools::install_github("GuangchuangYu/DOSE")
 devtools::install_github("GuangchuangYu/enrichplot")
 devtools::install_github("GuangchuangYu/clusterProfiler")
-                 
-bioPackages = 
-  c( 
+
+bioPackages =
+  c(
     "GDCRNATools", ##
-    "dplyr", "stringi", "purrr", ## 
+    "dplyr", "stringi", "purrr", ##
     "R.utils", "data.table", ## unzip and read table
     "GEOquery", ## download
     "FactoMineR", "factoextra", "ggfortify", ## PCA
@@ -162,27 +162,27 @@ bioPackages =
     "RTCGA","RTCGA.rnaseq","RTCGA.clinical","RTCGA.mutations",
     "RTCGA.mRNA","RTCGA.miRNASeq","RTCGA.RPPA","RTCGA.CNV","RTCGA.methylation"
   )
-lapply( bioPackages, 
-  function(bioPackage) {
-    if ( !require( bioPackage, character.only = T ) ) {
-      CRANpackages = available.packages()
-      ## install packages by CRAN
-      if ( bioPackage %in% rownames( CRANpackages ) ) {
-        install.packages( bioPackage )
-      }else{
-        ## install packages by bioconductor
-        ## R version >= 3.5 ===> BiocManager
-        if ( as.character( sessionInfo()$R.version$minor ) >= 3.5 ) {
-          if (!requireNamespace("BiocManager", quietly = TRUE))
-            install.packages("BiocManager")
-          BiocManager::install(bioPackage, update = TRUE, ask = FALSE)
-        }else{
-          ## R version < 3.5 ===> BiocInstaller
-          if (!requireNamespace("BiocInstaller", quietly = TRUE))
-            source( "https://bioconductor.org/biocLite.R" )
-          BiocInstaller::biocLite( bioPackage, ask = FALSE)
-        }
-      }
-    }
-  }
+lapply( bioPackages,
+       function(bioPackage) {
+         if ( !require( bioPackage, character.only = T ) ) {
+           CRANpackages = available.packages()
+           ## install packages by CRAN
+           if ( bioPackage %in% rownames( CRANpackages ) ) {
+             install.packages( bioPackage )
+           }else{
+             ## install packages by bioconductor
+             ## R version >= 3.5 ===> BiocManager
+             if ( as.character( sessionInfo()$R.version$minor ) >= 3.5 ) {
+               if (!requireNamespace("BiocManager", quietly = TRUE))
+                 install.packages("BiocManager")
+               BiocManager::install(bioPackage, update = TRUE, ask = FALSE)
+             }else{
+               ## R version < 3.5 ===> BiocInstaller
+               if (!requireNamespace("BiocInstaller", quietly = TRUE))
+                 source( "https://bioconductor.org/biocLite.R" )
+               BiocInstaller::biocLite( bioPackage, ask = FALSE)
+             }
+           }
+         }
+       }
 )
