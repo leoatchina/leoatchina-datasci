da08fa463e96e8323d3ca4ee82e92d26a8d467a1
leoatchina <leoatchina@gmail.com>
2022-11-07 18:04:27 +0800

    add push.sh

---
+++ push.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# File              : push.sh
+# Author            : leoatchina <leoatchina@outlook.com>
+# Date              : 2022.11.07
+# Last Modified Date: 2022.11.07
+# Last Modified By  : leoatchina <leoatchina@outlook.com>
+
+
+ for i in `docker images | grep leoatchina | head -n 2 | awk '{printf "%s:%s\n", $1,$2}'`; do echo $i; echo; docker push $i; done
