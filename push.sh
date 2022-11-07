5079a5a99acb407afd007906681a8a466d5e9555
leoatchina <leoatchina@gmail.com>
2022-11-07 18:31:05 +0800

    update push.sh

--- push.sh
+++ push.sh
@@ -6,4 +6,4 @@
 # Last Modified By  : leoatchina <leoatchina@outlook.com>
 
 
- for i in `docker images | grep leoatchina | head -n 2 | awk '{printf "%s:%s\n", $1,$2}'`; do echo $i; echo; docker push $i; done
+ for i in `docker images | grep leoatchina | head -n 2 | awk '{printf "%s:%s\n", $1,$2}' | sort -r`; do echo $i;docker push $i; done
